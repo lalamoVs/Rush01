800ce0c3822fcd6b316937d3a8c89e44bf652f28
lalamoVs <jasonwong22015@gmail.com>
2026-04-10 09:45:44 +0800

    Update

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -1,21 +1,44 @@
 #include <unistd.h>
 
-void double_digit(int m, int num){
-  char c;
-  if (m != 1){
-    double_digit(m - 1, num / 10);
-  }
-  c = (num % 10) + '0';
-  write(1, &c, 1);
+void print_comb(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        char c = a[i] + '0';
+        write(1, &c, 1);
+    }
+
+    if (a[0] != 10 - n)
+        write(1, ", ", 2);
 }
 
-void ft_print_combn(int n){
-  int a;
-  
+void ft_print_combn(int n)
+{
+    int a[10];
+    
+    for (int i = 0; i < n; i++)
+        a[i] = i;
+    while (1)
+    {
+        print_comb(a, n);
+        int i = n -1;
+
+        while (i >= 0 && a[i] == 10 - n + i){
+          i--;
+        }
+        if (i < 0){
+          break;
+        }
+        a[i]++;
+
+        for (int j = i + 1; j < n; j++){
+            a[j] = a[j - 1] + 1;
+        }
+    }
 }
 
 int main(){
-  ft_print_combn(3);
+  ft_print_combn(2);
   write(1, "\n", 1);
  return 0;
 }
